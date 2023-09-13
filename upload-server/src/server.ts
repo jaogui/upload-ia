a7e0cefe9e6d6020d48098c9160677e1c8c5d0b0
João Guilherme <joaobenine@gmail.com>
2023-09-12 21:52:40 -0300

    organizando estrutura server

--- upload-server/src/server.ts
+++ upload-server/src/server.ts
@@ -2,10 +2,6 @@ import { fastify } from 'fastify'
 import  {prisma } from './lib/prisma'
 
 const app = fastify();
-app.get('/prompts', async ()=>{
-  const prompts = await prisma.prompt.findMany()
-  return prompts
-})
 
 app.listen({
   port: 3333, 
