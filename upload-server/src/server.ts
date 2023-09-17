62509758ec791ba98ef0450f6bfc2684410d6b80
João Guilherme <joaobenine@gmail.com>
2023-09-16 22:23:00 -0300

    resolvendo erro axios add fastify corss

--- upload-server/src/server.ts
+++ upload-server/src/server.ts
@@ -1,10 +1,16 @@
 import { fastify } from "fastify";
+import {fastifyCors} from "@fastify/cors";
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoPost } from "./routes/upload-video";
 import { createTranscriptionRoute } from "./routes/create-transcription";
 import { generateCompletionRoute } from "./routes/generate-completion";
 
 const app = fastify();
+
+app.register(fastifyCors, {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+});
 
 app.register(getAllPromptsRoute);
 app.register(uploadVideoPost);
