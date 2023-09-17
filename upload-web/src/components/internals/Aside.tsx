95eb372f3e99e6f4f873acc032515951ec950e5b
João Guilherme <joaobenine@gmail.com>
2023-09-17 00:15:11 -0300

    registrando prompt no bd + status view ux

--- upload-web/src/components/internals/Aside.tsx
+++ upload-web/src/components/internals/Aside.tsx
@@ -16,9 +16,17 @@ import { getFFmpeg } from "../../lib/ffmpeg";
 import { fetchFile } from "@ffmpeg/util";
 import { api } from "../../lib/axios";
 
+type Status = "waiting" | "converting" | "uploading" | "generating" | "success";
+const menssageStatus = {
+  converting: "Convertendo.",
+  generating: "Transcrevendo..",
+  uploading: "Carregando...",
+  success: "Sucesso!",
+};
 export function Aside() {
   const [videoFile, setVideoFile] = useState<File | null>(null);
   const promptInputRef = useRef<HTMLTextAreaElement>(null);
+  const [status, setStatus] = useState<Status>("waiting");
 
   //Select vídeo
   function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
@@ -68,7 +76,7 @@ export function Aside() {
     const data = await ffmpeg.readFile("output.mp3");
     const audioFileBlob = new Blob([data], { type: "audio/mpeg" });
     const audioFile = new File([audioFileBlob], "audio.mp3", {
-      type: 'audio/mpeg'
+      type: "audio/mpeg",
     });
     console.log("convert finish");
     return audioFile;
@@ -78,22 +86,28 @@ export function Aside() {
   async function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    //Valor do vídeo
+    //Text area(Descrição)
     const prompt = promptInputRef.current?.value;
+    console.log(prompt);
 
     if (!videoFile) {
-      return
+      return;
     }
+    setStatus("converting");
 
     //Salvando e chamando conversão de vídeo em MP3
     const audioFile = await convertVideoToAudio(videoFile);
-    // console.log(audioFile)
     //Setando dados no back-end
-    const data = new FormData()
-    data.append('file', audioFile)  
-    const response = await api.post('/videos', data)
-    console.log(response)
-
+    const data = new FormData();
+    data.append("file", audioFile);
+    setStatus("uploading");
+    const response = await api.post("/videos", data);
+    const videoId = response.data.video.id;
+    setStatus("generating");
+    await api.post(`/videos/${videoId}/transcription`, {
+      prompt,
+    });
+    setStatus("success");
   }
 
   return (
@@ -131,10 +145,22 @@ export function Aside() {
             id="transcription_prompt"
             className="min-h-28 leading-relaxed resize-none"
             placeholder="Inclua as chaves mencionadas no vídeo."
+            disabled={status !== "waiting"}
           />
-          <Button type="submit" className="w-full bg-zinc-800">
-            Carregar vídeo
-            <UploadIcon className="h-4 w-4 ml-2" />
+          <Button
+            data-success={status === "success"}
+            disabled={status !== "waiting"}
+            type="submit"
+            className="w-full data-[success=true]:bg-green-400"
+          >
+            {status === "waiting" ? (
+              <>
+                Carregar video
+                <UploadIcon className="w-4 h-4 ml-2" />
+              </>
+            ) : (
+              menssageStatus[status]
+            )}
           </Button>
         </div>
       </form>
